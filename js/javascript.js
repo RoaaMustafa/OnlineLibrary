9fba1b3c16021806dc152667a92a25795fba3efd
RoaaMustafa <labushanab14@gmail.com>
2021-04-04 16:40:59 +0300

    adding js

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+'use scrict';
+let userName = prompt('What\'s your name ?');
+let age = prompt('what\'s your age ?');
+alert ('Welcome ' + userName + ' This is your age : ' + age);
+let bestsubject = prompt('What\'s your Favorite subject ?','computerscience, technology, science, space');
+alert( userName + ' you like  ' + bestsubject +'!');
+  let background = prompt('What\'s your Background?');
+  alert(background + 'is ausome!');
+
+{console.log('Hello ' + userName);
+console.log(typeof age);
+console.log(typeof background);
+}
